591ab2754076957d75139fb9a9ed4d55f1e0bc91
je-martini <jesusmartini2017@gmail.com>
2022-10-22 12:41:35 -0400

    writing the display's logic

--- index.js
+++ index.js
@@ -18,11 +18,149 @@ const equal_buttom = document.querySelector('.equal_buttom');
 const percentage = document.querySelector('.percentage');
 const zero = document.querySelector('.zero');
 const point = document.querySelector('.point');
-let math;
 
-math = Number(one.textContent) + Number(plus.textContent) + Number(two.textContent)
-console.log(math)
-// one.addEventListener('click', number)
+let previus_number_display = document.createElement('p')
+let next_number_display = document.createElement('p')
+let previus_number = 0;
+let next_number = 0;
+let is_previus_number_available = true;
+
+
+output.append(previus_number_display, next_number_display)
+
+zero.addEventListener('click', math_zero)
+one.addEventListener('click', math_one) 
+two.addEventListener('click', math_two) 
+three.addEventListener('click', math_three) 
+four.addEventListener('click', math_four) 
+five.addEventListener('click', math_five) 
+six.addEventListener('click', math_six) 
+seven.addEventListener('click', math_seven) 
+eight.addEventListener('click', math_eight) 
+nine.addEventListener('click', math_nine) 
+
+
+function math_zero() {
+    let zero_display = zero.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + zero_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + zero_display;
+    }
+}
+function math_one() {
+    let one_display = one.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + one_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + one_display;
+    }
+}
+
+function math_two() {
+    let two_display = two.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + two_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + two_display;
+    }
+}
+function math_three() {
+    let three_display = three.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + three_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + three_display;
+    }
+}
+function math_four() {
+    let four_display = four.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + four_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + four_display;
+    }
+}
+function math_five() {
+    let five_display = five.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + five_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + five_display;
+    }
+}
+function math_six() {
+    let six_display = six.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + six_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + six_display;
+    }
+}
+function math_seven() {
+    let seven_display = seven.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + seven_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + seven_display;
+    }
+}
+function math_eight() {
+    let eight_display = eight.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + eight_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + eight_display;
+    }
+}
+function math_nine() {
+    let nine_display = nine.textContent;
+    if( is_previus_number_available) {
+        previus_number_display.innerHTML = previus_number_display.textContent + nine_display;
+    } else {
+        next_number_display.innerHTML = next_number_display.textContent + nine_display;
+    }
+}
+
+
+plus.addEventListener('click', adding)
+function adding(){
+    let plus_display = plus.textContent;
+    previus_number_display.innerHTML = previus_number_display.textContent + plus_display;
+    is_previus_number_available = false
+}
+
+equal_buttom.addEventListener('click', final_result)
+
+function final_result(){
+    
+}
+
+
+
+// two.addEventListener('click', math) 
+
+// function math(number) {
+//     console.log('hi')
+//     if(is_previus_number){
+//         console.log('no')
+//         let uno = number
+//         console.log(uno)
+//         previus_number = Number(number.textContent)
+//         is_previus_number = false
+//         console.log(previus_number)
+//     } else {
+//         console.log('yes')
+//         let dos = number
+//         console.log(dos)
+//         next_number = Number(number.textContent)
+//         is_previus_number = true
+//         console.log(next_number)
+
+//     }
+
+// }
+
 
 // function number () {
 //     math = Number(one.textContent) 
